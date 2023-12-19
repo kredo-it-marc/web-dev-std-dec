52947bf9f7e32c09e31ac0b35abeb7db8603c8ac
kredo-it-marc <83500146+kredo-it-marc@users.noreply.github.com>
2023-12-19 21:23:33 +0800

    Day 16

---
+++ minimart-catalog/connection.php
@@ -0,0 +1,27 @@
+<?php
+    function dbConnect()
+    {
+        $servername = "localhost";
+        $username = "marc"; //MAMP and XAMPP uses "root"
+        $password = "password"; //MAMP uses "root" for the password. XAMPP doesn't have a password.
+        $db_name = "minimart_catalog";
+        
+        $conn = new mysqli($servername, $username, $password, $db_name);
+        // new mysqli($servername, $username, $password, $db_name) 
+        // this creates a connection to the database.
+        // This code returns what we call an Object.
+        //When you say an object, it has variables and functions in it.
+        //On line 7, the connection to the database is assign to $conn
+    
+        if($conn->connect_error) //check if there is an error in the connection
+        {
+            die("Connection unsuccessful: ". $conn->connect_error);
+        }
+        else
+        {
+            // echo "connection successful";
+            return $conn; //return the connection to the database
+        }
+    }
+
+?>
